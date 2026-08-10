19d2c0c97aefb14d89d6d1db52597be73498f82c
doudoubenlamlih-max <doudoubenlamlih@gmail.com>
2026-08-11 00:31:34 +0200

    done

---
+++ app/Models/Bid.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bid extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'amount',
+        'is_winning'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
