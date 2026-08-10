cec2ee3d7e94fbca1d0814e9d19909ad32994dba
doudoubenlamlih-max <doudoubenlamlih@gmail.com>
2026-08-11 00:33:24 +0200

    done

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'bid_id',
+        'total_amount',
+        'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function bid()
+    {
+        return $this->belongsTo(Bid::class);
+    }
+
 }
