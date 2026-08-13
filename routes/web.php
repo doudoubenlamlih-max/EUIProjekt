bea269f08343c8b3fb630871ed298ef9ec4186bb
doudoubenlamlih-max <doudoubenlamlih@gmail.com>
2026-08-13 15:36:29 +0200

    newGebot

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BidController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', ProductController::class);
+Route::post('/products/{product}/bids', [BidController::class, 'store'])
+    ->name('bids.store');
