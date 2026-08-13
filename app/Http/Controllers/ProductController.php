9fb076d7b0218c39a27361cd11dde4b1d8865e96
doudoubenlamlih-max <doudoubenlamlih@gmail.com>
2026-08-13 15:12:20 +0200

    Add product detail action

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -33,11 +33,12 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
-    }
+  public function show(string $id)
+{
+    $product = Product::findOrFail($id);
 
+    return view('products.show', compact('product'));
+}
     /**
      * Show the form for editing the specified resource.
      */
