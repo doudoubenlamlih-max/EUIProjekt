13e409ac74dc90f3c26b0969bc00fb995c6b7191
doudoubenlamlih-max <doudoubenlamlih@gmail.com>
2026-08-14 15:56:33 +0200

    CurrentPreisinProductmodel

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+  protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'description',
+        'starting_price',
+        'current_price',
+        'image',
+        'status',
+        'current_price',
+        'ends_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function bids()
+    {
+        return $this->hasMany(Bid::class);
+    }
 }
